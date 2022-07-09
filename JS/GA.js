a27e8504a5ce8a05736e9ebb78b5c91ff507af20
ravenwhite11 <55204078+ravenwhite11@users.noreply.github.com>
2022-07-09 11:58:42 +0800

    Update GA.js

--- JS/GA.js
+++ JS/GA.js
@@ -1,8 +1,9 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-17JJPG1WKV"></script>
-	<script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-17JJPG1WKV');
-  </script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-17JJPG1WKV"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-17JJPG1WKV');
+</script>
